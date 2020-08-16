fa7fff3d0f2462bb314aa46ac4a4d06d80a80856
cond-freakz <marampilly.sarath@gmail.com>
2020-08-16 13:49:40 +0530

    test

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -8,19 +8,13 @@ const router = Router();
 router.route('/login').post(Auth.login);
 router.route('/details').post(Auth.userDetails);
 
-router.get(
-  '/google',
-  passport.authenticate('google', {
-    scope: [
-      'https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/userinfo.email',
-    ],
-  }),
-);
+router.get('/google', (req, res) => {
+  return res.status(404);
+});
 
 router.get(
   '/google/signin',
-  //passport.authenticate('google', { failureRedirect: `${app.webUrl}/login` }),
+  passport.authenticate('google', { failureRedirect: `${app.webUrl}/login` }),
   (req, res) => {
     console.log(req.user);
     res.redirect(`${app.webUrl}/`);
