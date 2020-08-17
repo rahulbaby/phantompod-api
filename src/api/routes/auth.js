f05686ddce1a14d5df6bcff6f6aca9dc84bc8779
Anoop TD <anooptd@Anoops-MacBook-Pro.local>
2020-08-17 22:44:45 +0530

    change 123

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -34,18 +34,16 @@ router.get(
 
 router
   .route('/google/signin')
-  .get(passport.authenticate('google', {failureRedirect:'/login'}),(req,res)=>{
-    console.log('user :',req);
-  })
-  // , (req, res) => {
-  //   try {
-  //     console.log('user', req);
-  //     return res.send({ user: req.user, user2: 'hell' });
-  //   } catch (error) {
-  //     console.log('error', error);
-  //     let message = error.message || `Something went wrong!`;
-  //     return res.status(400).send({ message, error });
-  //   }
-  // });
+  .get(passport.authenticate('google', {failureRedirect:'/login'}),(req, res) => {
+    try {
+      console.log('user', req);
+      return res.send({ user: req.user, user2: 'hell' });
+    } catch (error) {
+      console.log('error', error);
+      let message = error.message || `Something went wrong!`;
+      return res.status(400).send({ message, error });
+    }
+  });
+  
 
 export default router;
