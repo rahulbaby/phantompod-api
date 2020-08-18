70e9356950910e2cb4fa51ead752c969a4aa7e65
cond-freakz <marampilly.sarath@gmail.com>
2020-08-18 21:46:40 +0530

    g-Auth medium

--- src/api/controllers/dashboard.js
+++ src/api/controllers/dashboard.js
@@ -7,7 +7,7 @@ class BlockController {
 	index = async (req, res, next) => {
 		try {
 			const userId = req.user.id;
-			const profileViews = req.user.profileViews;
+			const profileViews = req.user.profileViews || 0;
 			const podsOwn = await Pod.where({ userId }).countDocuments();
 			const podsImIn = await Pod.where({
 				members: { $elemMatch: { userId: userId, status: podMemeberStatus.ACCEPTED } },
@@ -21,7 +21,7 @@ class BlockController {
 				},
 			]);
 
-			return res.send({ podsOwn, podsImIn, profileViews, postLikes: postLikes.count });
+			return res.send({ podsOwn, podsImIn, profileViews, postLikes: postLikes.count || 0 });
 		} catch (error) {
 			let message = error.message || `Something went wrong fetching stastics!`;
 			return res.status(400).send({ message, error });
