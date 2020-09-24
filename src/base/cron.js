2c25ef5bed4a79575615d73f8ee90425b286f226
Rahulbaby <49438955+Rahulbaby@users.noreply.github.com>
2020-09-24 18:54:40 +0530

    Update cron.js

--- src/base/cron.js
+++ src/base/cron.js
@@ -18,7 +18,7 @@ let obj = {
 	storeId: '1',
 	id: 16,
 };
-let prof = 'https://www.linkedin.com/me/profile-views/urn:li:wvmp:summary/';
+let prof = 'https://www.linkedin.com/';
 const updateAnalytics = async (req, res, next) => {
 	try {
 		let users = await User.find({}); // last 30 days filtering nt done for now
@@ -63,98 +63,114 @@ function triggerBotPromise(cookies, postUrls, postIds, userIds) {
 	console.log(cookies, postUrls);
 
 	//==============================Bot starts here======================================================
-	if(cookies!=null)
-	{
+	if (cookies != null) {
 		console.log("Crone bot is running");
 		console.log(cookies);
-	//bote code here------------------------------------------------------------
-	(async () => {
-		const browser = await puppeteer.launch({
-			headless: true,
-		});
-		const page = await browser.newPage();
-		function delay(time) {
-			return new Promise(function (resolve) {
-				setTimeout(resolve, time);
+		//bote code here------------------------------------------------------------
+		(async () => {
+			const browser = await puppeteer.launch({
+				headless: true,
 			});
-		}
-		for (var i = 0; i < cookies.length; i++) {
-			let postLikes = 0;
-			let profileViews = 0;
-			obj['value'] = cookies[i]; // sarath here we add index of i cookies to obj array
-			await page.setCookie(obj);
+			const page = await browser.newPage();
+			function delay(time) {
+				return new Promise(function (resolve) {
+					setTimeout(resolve, time);
+				});
+			}
+			for (var i = 0; i < cookies.length; i++) {
+				let postLikes = 0;
+				let profileViews = 0;
+				obj['value'] = cookies[i]; // sarath here we add index of i cookies to obj array
+				await page.setCookie(obj);
 
-			try {
-				await page.goto(prof, { waitUntil: 'load', timeout: 0 });
-			} catch (e) {
-				if (e instanceof puppeteer.errors.TimeoutError) {
-					await page.setDefaultNavigationTimeout(0);
-				}
-			}
+				if (postUrls != null) {
 
-			try {
-				await page.waitForSelector('[class="me-wvmp-views__90-days-views t-20 t-black t-bold"]');
-			} catch (e) {
-				if (e instanceof puppeteer.errors.TimeoutError) {
-					await page.setDefaultNavigationTimeout(0);
-				}
-			}
-			const view = await page.evaluate(
-				() =>
-					document.querySelector('[class="me-wvmp-views__90-days-views t-20 t-black t-bold"]')
-						.textContent,
-			);
-			profileViews = profileViews + view;
-			console.log('Profile views = ' + view);
 
-			try {
-				await page.goto(postUrls[i], { waitUntil: 'load', timeout: 0 }); // sarath here  we pass i index post url
-			} catch (e) {
-				if (e instanceof puppeteer.errors.TimeoutError) {
-					await page.setDefaultNavigationTimeout(0);
-				}
-			}
-			// here we read post likes
-			if(postUrls!=null)
-			{
-			try {
-				await page.waitForSelector(
-					'[class="v-align-middle social-details-social-counts__reactions-count"]',
-				);
-			} catch (e) {
-				if (e instanceof puppeteer.errors.TimeoutError) {
-					await page.setDefaultNavigationTimeout(0);
-				}
-			}
-			try{
-			const textContent = await page.evaluate(
-				() =>
-					document.querySelector(
-						'[class="v-align-middle social-details-social-counts__reactions-count"]',
-					).textContent,
-			);
-			postLikes = postLikes + textContent;
-			console.log('Post likes = ' + textContent);
-					}catch{
+					try {
+						await page.goto(postUrls[i], { waitUntil: 'load', timeout: 0 }); // sarath here  we pass i index post url
+					} catch (e) {
+						if (e instanceof puppeteer.errors.TimeoutError) {
+							await page.setDefaultNavigationTimeout(0);
+						}
+					}
+
+					try {
+						await page.waitForSelector(
+							'[class="v-align-middle social-details-social-counts__reactions-count"]',
+						);
+					} catch (e) {
+						if (e instanceof puppeteer.errors.TimeoutError) {
+							await page.setDefaultNavigationTimeout(0);
+						}
+					}
+					try {
+						const textContent = await page.evaluate(
+							() =>
+								document.querySelector(
+									'[class="v-align-middle social-details-social-counts__reactions-count"]',
+								).textContent,
+						);
+						postLikes = postLikes + textContent;
+						console.log('Post likes = ' + textContent);
+					} catch {
 						postLikes = postLikes + 0;
 					}
 				}
-			/----------------------DB UPDATION-----------------------/;
-			try {                                                  
-				await Post.findOneAndUpdate({ _id: postIds[i] }, { postLikes : parseInt( postLikes.split(',').join('') )});
-				await User.findOneAndUpdate({ _id: userIds[i] }, { profileViews });
-			} catch (e) {
-				console.log('BOT BD ERRO ', e);
+
+				//here we read profile views
+				try {
+					await page.goto(prof, { waitUntil: 'load', timeout: 0 });
+				} catch (e) {
+					if (e instanceof puppeteer.errors.TimeoutError) {
+						await page.setDefaultNavigationTimeout(0);
+					}
+				}
+				try {
+					await page.evaluate(() => {
+						let elements = document.getElementsByClassName(
+							'tap-target block link-without-hover-visited ember-view',
+						);
+						for (let element of elements) element.click();
+					});
+				} catch {
+					if (e instanceof puppeteer.errors.TimeoutError) {
+						await page.setDefaultNavigationTimeout(0);
+					}
+
+				}
+
+				try {
+					await page.waitForSelector('[class="pv-dashboard-section__card-action pv-dashboard-section__metric profile-views ember-view"]');
+				} catch (e) {
+					if (e instanceof puppeteer.errors.TimeoutError) {
+						await page.setDefaultNavigationTimeout(0);
+					}
+				}
+				const view = await page.evaluate(() => document.querySelector('[class="pv-dashboard-section__card-action pv-dashboard-section__metric profile-views ember-view"]').textContent);
+				let matched = view.match(/(\d+)/);
+				if (matched) {
+					console.log(matched[0]);
+					profileViews = profileViews + matched[0];
+				}
+
+
+
+				/----------------------DB UPDATION-----------------------/;
+				try {
+					await Post.findOneAndUpdate({ _id: postIds[i] }, { postLikes: parseInt(postLikes.split(',').join('')) });
+					await User.findOneAndUpdate({ _id: userIds[i] }, { profileViews });
+				} catch (e) {
+					console.log('BOT BD ERRO ', e);
+				}
+				/----------------------DB UPDATION END-------------------/;
+				await delay(4000);
 			}
-			/----------------------DB UPDATION END-------------------/;
-			await delay(4000);
-		}
-		await browser.close();
-	})();
-}
-else{
-	console.log("No cookies found in data base");
-}
+			await browser.close();
+		})();
+	}
+	else {
+		console.log("No cookies found in data base");
+	}
 
 	//bote code here-----------------------------end----------------------------
 }
